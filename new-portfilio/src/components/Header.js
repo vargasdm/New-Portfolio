25c7be1986193c9824ed86e845c699219eeb63e5
vargasdm <112888008+vargasdm@users.noreply.github.com>
2023-03-12 15:56:49 -0400

    added styling to nav bar

--- new-portfilio/src/components/Header.js
+++ new-portfilio/src/components/Header.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import '../styles/Header.css';
-import Resume from '../assets/documents/Vargas-General-Resume.pdf'
 // Here we are using object destructuring assignment to pluck off our variables from the props object
 // We assign them to their own variable names
 function NavTabs({ currentPage, handlePageChange }) {
--- new-portfilio/src/utils/helpers.js
+++ new-portfilio/src/utils/helpers.js
@@ -6,11 +6,3 @@ export function validateEmail(email) {
     }
     return false;
 }
-
-export function checkPassword(input) {
-    const passw = /^[A-Za-z]\w{7,14}$/;
-    if (input.match(passw)) {
-      return true;
-    }
-    return false;
-  }
